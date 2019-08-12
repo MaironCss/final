4e93352566f33c9664d5f58f6a8d588041a9490c
MaironCss <mironk96@mail.ru>
2019-08-12 23:33:56 +0300

    add adaptive

--- js/app.js
+++ js/app.js
@@ -5,13 +5,9 @@ $('.ba-slider').slick({
   speed: 500,
   fade: true,
   arrows: true,
-  cssEase: 'linear'
-});
-$('.ba-carusel').slick({
-  centerMode: true,
-  centerPadding: '60px',
-  slidesToShow: 3,
-  responsive: [
+  cssEase: 'linear',
+   adaptiveHeight: true,
+    responsive: [
     {
       breakpoint: 768,
       settings: {
@@ -32,4 +28,43 @@ $('.ba-carusel').slick({
     }
   ]
 });
+$('.ba-carusel').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  arrows: true,
+    adaptiveHeight: true,
+     responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+
+});
 				
+
+$('.btn-menu').on('click', function(){
+
+  $('.nav-menu').toggleClass('active');
+
+})
+
+$('.menu a').on('click', function(e){
+  // e.preventDefault();
+  $('.nav-menu').removeClass('active');
+})
